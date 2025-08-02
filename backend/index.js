990d22c010d1934df5f54bce142f57b708826736
Shahab Dad <shahabdad@gmail.com>
2025-08-01 23:57:41 -0700

    cleaning in code

--- backend/index.js
+++ backend/index.js
@@ -4,26 +4,10 @@ const  dotenv = require('dotenv');
 const authRoutes  = require('./routes/authRoutes');
 const connectDB = require('./config/db');
 
-
 dotenv.config();
 const app = express();
-
-// connectDB();
-
 app.use(express.json());  
 app.use('/api/auth', authRoutes); 
-
-
-
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(() => {
-//         app.listen(5000, () => {
-//             console.log('Server is running on http://localhost:5000');
-//         });
-//     })
-//     .catch((err) => {
-//         console.error('MongoDB connection error:', err.message);
-//     });
 
 
     connectDB().then(() => {
--- backend/model/User.js
+++ backend/model/User.js
@@ -1,16 +1,5 @@
-// const mongoose = require('mongoose')
-// // const bcrypt =  require('bcryptjs')
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-
-
-// const userSchema  = new mongoose.Schema({ 
-//     name:String,
-//     email: {type: String, unique: true}, 
-//     password: String,
-// });
-
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -22,15 +11,5 @@ userSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
-
-//  Hash the password before saving 
-
-// userSchema.pre('save' , async function (next) { 
-//     if (!this.isModified('password') ) return  next();
-//     this.password = await bcrypt.hash(this.password, 10); 
-//     next(); 
-
-
-// });
 
 module.exports = mongoose.model('User', userSchema); 
